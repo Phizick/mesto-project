a33d6c414ec556051dd4129c6e38aef2e095d4a3
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-06-23 16:56:04 +0300

    before package

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-
+import {initialCards} from './cards.js'
 const popupProfileForm = document.querySelector(".popup__form-profile");
 const profileEdit = document.querySelector(".profile__name-edit"); 
 const popupProfile = document.querySelector(".popup__profile"); 
@@ -16,7 +16,11 @@ const galleryList = document.querySelector(".gallery__grid");
 const galleryTemplate = document.querySelector(".gallery__template").content;
 const popupOpenedImg = document.querySelector(".popup__img-opened");
 
-const formElement = document.querySelector('.popup__form')
+
+// const popupOpenedForm = document.querySelector('.popup_opened')
+// const formElement = document.querySelector('.popup__form')
+// const formElement = document.getElementById('123')
+
 
 
 export {imgLinkInput, imgNameInput, galleryList, galleryTemplate, popupOpenedImg, openPopup, closePopup, keyClose, overlayClose}
@@ -25,7 +29,9 @@ const openPopup = (item => {
   item.classList.add("popup_opened");
   item.addEventListener('click', overlayClose);
   document.addEventListener('keydown', keyClose);
+  enableValidation()
 });
+
 
 
 const closePopup = (item => {
@@ -60,15 +66,14 @@ const validationConfig = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
   inactiveButtonClass: 'popup__save-button_disabled',
-  inputErrorClass: '.popup__input_type_error',
-  errorClass: '.popup__input-error-active',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error-active',
   formButtonSubmit: '.popup__save-button'
 };
 
 const showInputError = (formElement, inputElement, validationConfig) => {
   const { inputErrorClass, errorClass } = validationConfig;
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  console.log(errorElement)
   inputElement.classList.add(inputErrorClass);
   errorElement.textContent = inputElement.validationMessage;
   errorElement.classList.add(errorClass);
@@ -91,11 +96,8 @@ const checkInputValidity = (formElement, inputElement) => {
     hideInputError(formElement, inputElement, validationConfig);       
   }
 };
-const inputList = Array.from(document.querySelectorAll('.popup__input'));
 
-const invalidInput = (inputList) => {
-  return inputList.some(inputElement => !inputElement.validity.valid);
-}
+
 
 const buttonCondition = (buttonSelected, inputList, validationConfig) => {
   
@@ -111,7 +113,8 @@ const buttonCondition = (buttonSelected, inputList, validationConfig) => {
 }
 
 const setEventListeners = (formElement, validationConfig) => {
-  const buttonSelected = formElement.querySelector('.popup__save-button');
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonSelected = formElement.querySelector('.popup__save-button'); 
   const {inputSelector, submitButtonSelector, ...anyConfig} = validationConfig;
   formElement.addEventListener('submit', evt => {evt.preventDefault()})
   
@@ -125,6 +128,12 @@ const setEventListeners = (formElement, validationConfig) => {
 }
 
 
+const invalidInput = (inputList) => {
+  return inputList.some(inputElement => !inputElement.validity.valid);
+}
+
+
+
 const enableValidation = () => {
   const {formSelector, ...anyConfig} = validationConfig;
   const formList = Array.from(document.querySelectorAll(formSelector));
@@ -133,7 +142,6 @@ const enableValidation = () => {
   })
 }
 
-enableValidation()
 
 formProfileSaveBtn.addEventListener("submit", evt => {
   evt.preventDefault();
@@ -144,8 +152,54 @@ formProfileSaveBtn.addEventListener("submit", evt => {
 
 popupAddCard.addEventListener("submit", (evt) => {
   evt.preventDefault();
+
   renderCard(imgNameInput.value, imgLinkInput.value);
   closePopup(popupAddCard);    
   imgNameInput.value = "";
   imgLinkInput.value = "";
+});
+
+
+//создание новой карточки
+const createNewCard = (cardName, cardLink) => {
+  //создание контейнера под карточку и определение его значений
+  const card = galleryTemplate.querySelector(".gallery__grid-item").cloneNode(true);
+  const image = card.querySelector(".gallery__grid-image");
+  image.src = cardLink;
+  image.alt = cardName;
+  card.querySelector(".gallery__grid-name").textContent = cardName;
+  
+  //удаление карточки
+  const delItem = card.querySelector(".gallery__delete-img-button");
+  delItem.addEventListener("click", () => {
+      const listItem = delItem.closest(".gallery__grid-item");
+      listItem.remove();
+  });
+
+  //лайк карточки
+  const likebtn = card.querySelector(".gallery__grid-like");
+  likebtn.addEventListener("click", () => {
+      likebtn.classList.toggle("gallery__grid-like_active");
+  }); 
+
+  //открытие карточки в полном размере изображения
+  const imageContainer = document.querySelector(".image__container");
+  image.addEventListener("click", () => {
+      const imageOpened = imageContainer.querySelector(".image__opened");
+      imageOpened.src = image.src;
+      imageOpened.alt = image.alt;
+      imageContainer.querySelector(".image__opened-title").textContent = image.alt;
+      openPopup(popupOpenedImg);
+  });
+  return card;
+};
+
+//отрисовка карточки в контейнер
+const renderCard = (cardName, cardLink) => {
+  galleryList.prepend(createNewCard(cardName, cardLink));    
+};
+
+//перебор массива
+initialCards.forEach(item => {
+  renderCard(item.name, item.link);
 });
