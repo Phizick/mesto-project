449b548cd2d21a997eeeb4b63792c29a25565ddf
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-07-23 11:41:33 +0300

    Update popupWithImage.js

--- src/components/popupWithImage.js
+++ src/components/popupWithImage.js
@@ -1,5 +1,19 @@
 import Popup from "./popup";
 
 export default class PopupWithImage extends Popup {
+    constructor(popupSelector) {
+        super(popupSelector);
+        this._figcaption = this._popup.querySelector('.image__opened-title');
+        this._imageOpened = this._popup.querySelector('.image__opened');
+    }
+
+    open(name, link) {
+        super.open();
+        this._name = name;
+        this._link = link;
+        this._figcaption.textContent = this._name;
+        this._imageOpened.src = this._link;
+        this._imageOpened.alt = this._name;
+    }
 
 }
